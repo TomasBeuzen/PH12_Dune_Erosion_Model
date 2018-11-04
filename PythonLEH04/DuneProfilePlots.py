9c286759a01f1581884365d106df4a62b8d3295b
Evan B. Goldstein <ebgoldstein@users.noreply.github.com>
2018-11-03 22:40:06 -0400

    plot profiles

---
+++ PythonLEH04/DuneProfilePlots.py
@@ -0,0 +1,51 @@
+
+"""
+Script to make plots of the dune profile
+
+@author: evanbgoldstein
+"""
+
+
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+from LEH04 import LEH04ensembles
+
+with open('DIM_data_2011.pkl', 'rb') as f:
+    data = pickle.load(f)
+
+#profile #
+p=15;
+#number of ensembles
+ens=10;
+
+profile=data[p]
+    
+#pull all relevant data out of the dictionary
+dv = profile['dv']
+zb = profile['zb']
+T = profile['Tp']
+R_st = profile['R_st']
+R_gp = profile['R_gp']
+R_gp_draws = profile['R_gp_draws']
+
+#run it through GP
+[SigDuneErosionGP,zbmGP] = LEH04ensembles(dv,zb,R_gp,T)
+#run it through 10 'draws' from GP
+[SigDuneErosionGPD,zbmGPD] = LEH04ensembles(dv,zb,R_gp_draws,T)
+
+
+#plotting
+t = np.arange(0, 120)
+fig, axs = plt.subplots(2, 1)
+axs[0].plot(t, R_gp_draws[:,0:11],'gray',t, R_gp,'b', t,zbmGPD[:,0:11],'gray',t,zbmGP,'k',)
+
+axs[0].set_xlabel('time (hours)')
+axs[0].set_ylabel('Runup elevation (blue) and Dune Toe Height (black)')
+
+axs[1].plot(t, SigDuneErosionGPD[:,0:11],'gray', t, SigDuneErosionGP,'b')
+axs[1].set_ylabel('total delV')
+
+plt.show()
+
